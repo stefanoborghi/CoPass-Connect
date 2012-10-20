47ce7c509881a6026a5c39a3c77bc3ad5fae0b83
emmepig <emmepig@gmail.com>
2012-10-20 23:11:26 +0300

    Update root/library/database/cl_tba_cards.php
    aggiornata const TABLE_NAME e metodo $stpdo

--- root/library/database/cl_tba_cards.php
+++ root/library/database/cl_tba_cards.php
@@ -1,5 +1,5 @@
 <?php
-// CLASS TABLE USER ACCOUNT
+// CLASS CARDS
 
 class cl_tba_cards extends Entity_Object {
     /* CLASS VARIABLES */
@@ -9,17 +9,17 @@ class cl_tba_cards extends Entity_Object {
     private $id_oj;            # current card
     
     /* CONSTANT */
-    private static $t_name="tba_cards"; //  table name
+    const TABLE_NAME="tba_cards"; //  table name
     
-    private static $t_qry_id="SELECT * FROM tba_cards WHERE id=:id";   
-    private static $t_qry_owner="SELECT * FROM tba_cards WHERE tba_cc_site_id_generator=:id";   
-    private static $t_qry_gnerator="SELECT * FROM tba_cards WHERE tba_cc_site_id_owner=:id";   
+    const t_qry_id="SELECT * FROM tba_cards WHERE id=:id";   
+    const t_qry_owner="SELECT * FROM tba_cards WHERE tba_cc_site_id_generator=:id";   
+    const t_qry_gnerator="SELECT * FROM tba_cards WHERE tba_cc_site_id_owner=:id";   
     
-    private static $t_upd_status="UPDATE tba_cards SET status=:status WHERE id=:id";    
-    private static $t_upd_amount="UPDATE tba_cards SET credits_amount=:credits_amount WHERE id=:id";    
+    const t_upd_status="UPDATE tba_cards SET status=:status WHERE id=:id";    
+    const t_upd_amount="UPDATE tba_cards SET credits_amount=:credits_amount WHERE id=:id";    
     
     
-    private static $t_qry_insert="INSERT INTO tba_cards (id, 
+    const t_qry_insert="INSERT INTO tba_cards (id, 
                             tba_cc_site_id_generator,
                             tba_cc_site_id_owner,
                             credits_amount,
@@ -31,7 +31,7 @@ class cl_tba_cards extends Entity_Object {
                             :card_type,
                             :status)";   
 
-    private static $var_list=array(
+    const var_list=array(
                                    "tba_cc_site_id_generator"=>array("lab"=>"generator","must"=>true,"t"=>PDO::PARAM_INT),
                                    "tba_cc_site_id_owner"=>array("lab"=>"owner","must"=>true,"t"=>PDO::PARAM_INT),
                                    "credits_amount"=>array("lab"=>"amounts","must"=>true,"t"=>PDO::PARAM_INT),
@@ -52,15 +52,16 @@ class cl_tba_cards extends Entity_Object {
         $this->id_oj=$id;
         
         # parameter:
-        $stpdo = $this->mypdo->prepare(cl_tba_cards::t_qry_id);
+        $stpdo = $this->mypdo->prepare(self::t_qry_id);
         $stpdo->bindParam(':id', $id, PDO::PARAM_INT);
-
+        
+        $stpdo->execute();
         $arrcode = $stpdo->errorInfo();
         if($arrcode[0] == $this->transaction_ok){//query ok
             $this->var[$this->id_oj] = $stpdo->fetchAll(PDO::FETCH_ASSOC);
             return array('result'=>10,'value'=>$this->var[$this->id_oj]);
         }else{
-            $this->toLog("Errore in scl_tba_cards - query: ".cl_tba_cards::t_qry_id." - param: ".$id);
+            $this->toLog("Errore in scl_tba_cards - query: ".self::t_qry_id." - param: ".$id);
             return array('result'=>0);
         }
     }
@@ -68,10 +69,10 @@ class cl_tba_cards extends Entity_Object {
     #   insert new card
     function set($var_into) {
         $errors=array();
-        $stpdo = $this->mypdo->prepare(cl_tba_cards::t_qry_insert);
+        $stpdo = $this->mypdo->prepare(self::t_qry_insert);
         
         # verify parameter:
-        foreach(cl_tba_cards::$var_list as $id=>$val) {
+        foreach(self::$var_list as $id=>$val) {
             if ($val['must'] && (!isset($var_into[$id]) or (strlen(trim($var_into[$id]))==0))) {
                 $errors[$id]=$val["lab"]." required";
             }
@@ -86,12 +87,13 @@ class cl_tba_cards extends Entity_Object {
         $stpdo->bindParam(":id", $this->id_oj, PDO::PARAM_STR);
         
         #   try to insert new cards
+        $stpdo->execute();
         $arrcode = $stpdo->errorInfo();
         if($arrcode[0] == $this->transaction_ok){//query ok
             $row = $stpdo->fetchAll(PDO::FETCH_ASSOC);
             return $row;
         }else{
-            $this->toLog("Errore in cl_tba_cards - query: ".cl_tba_cards::t_qry_insert." - param: ".$var_into);
+            $this->toLog("Errore in cl_tba_cards - query: ".self::t_qry_insert." - param: ".$var_into);
             return array('result'=>0);
         }
     }
@@ -99,16 +101,17 @@ class cl_tba_cards extends Entity_Object {
     #   update status
     function set_status($id, $status) {
         $errors=array();
-        $stpdo = $this->mypdo->prepare(cl_tba_cards::t_upd_status);
+        $stpdo = $this->mypdo->prepare(self::t_upd_status);
         $stpdo->bindParam(":status", $status, PDO::PARAM_STR);
         $stpdo->bindParam(":id", $id, PDO::PARAM_STR);
         
+        $stpdo->execute();
         $arrcode = $stpdo->errorInfo();
         if($arrcode[0] == $this->transaction_ok){//query ok
             $row = $stpdo->fetchAll(PDO::FETCH_ASSOC);
             return $row;
         }else{
-            $this->toLog("Errore in cl_tba_cards - query: ".cl_tba_cards::t_upd_status." - param: ".$var_into);
+            $this->toLog("Errore in cl_tba_cards - query: ".self::t_upd_status." - param: ".$var_into);
             return array('result'=>0);
         }
     }
@@ -116,16 +119,17 @@ class cl_tba_cards extends Entity_Object {
     #   update amount
     function set_credits_amountt($id, $credits_amount) {
         $errors=array();
-        $stpdo = $this->mypdo->prepare(cl_tba_cards::t_upd_amount);
+        $stpdo = $this->mypdo->prepare(self::t_upd_amount);
         $stpdo->bindParam(":credits_amount", $credits_amount, PDO::PARAM_STR);
         $stpdo->bindParam(":id", $id, PDO::PARAM_STR);
         
+        $stpdo->execute();
         $arrcode = $stpdo->errorInfo();
         if($arrcode[0] == $this->transaction_ok){//query ok
             $row = $stpdo->fetchAll(PDO::FETCH_ASSOC);
             return $row;
         }else{
-            $this->toLog("Errore in cl_tba_cards - query: ".cl_tba_cards::t_upd_amount." - param: ".$var_into);
+            $this->toLog("Errore in cl_tba_cards - query: ".self::t_upd_amount." - param: ".$var_into);
             return array('result'=>0);
         }
     }
