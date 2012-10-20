8bbd594d7ad86be1a8a77af11bdb29c94965b51d
emmepig <emmepig@gmail.com>
2012-10-20 23:14:12 +0300

    Update root/library/database/tba_transactions_log.php
    aggiornata const TABLE_NAME e metodo $stpdo

--- root/library/database/tba_transactions_log.php
+++ root/library/database/tba_transactions_log.php
@@ -1,23 +1,21 @@
 <?php
-// CLASS TABLE USER ACCOUNT
+// CLASS TRANSACTION
 
 class cl_tba_transactions_log extends Entity_Object {
     /* CLASS VARIABLES */
     var $database = NULL;
     
     private $var=array();      # local variables
-    private $id_oj;            # current card
+    private $id_oj;            # current transaction
     
     /* CONSTANT */
-    const TABLE_NAME = 'table_name';
+    const TABLE_NAME = 'tba_transactions_log';
     
-    private static $t_name="tba_transactions_log"; //  table name
-
-    private static $t_qry_id="SELECT * FROM tba_transactions_log WHERE id=:id";   
+    const t_qry_id="SELECT * FROM tba_transactions_log WHERE id=:id";   
     
-    private static $t_upd_status="UPDATE cl_tba_transactions_log SET status=:status WHERE tba_cards_id=:id";    
+    const t_upd_status="UPDATE cl_tba_transactions_log SET status=:status WHERE tba_cards_id=:id";    
         
-    private static $t_qry_insert="INSERT INTO cl_tba_transactions_log (
+    const t_qry_insert="INSERT INTO cl_tba_transactions_log (
                             tba_cards_id,
                             tba_cc_site_id_start,
                             tba_transactios_type_id,
@@ -31,7 +29,7 @@ class cl_tba_transactions_log extends Entity_Object {
                             :credits,
                             :status)";   
 
-    private static $var_list=array(
+    const var_list=array(
                                    "tba_cards_id"=>array("lab"=>"card id","must"=>true,"t"=>PDO::PARAM_STR),
                                    "tba_cc_site_id_start"=>array("lab"=>"site di (start)","must"=>true,"t"=>PDO::PARAM_INT),
                                    "tba_transactios_type_id"=>array("lab"=>"tranaction type","must"=>true,"t"=>PDO::PARAM_STR),
@@ -53,15 +51,16 @@ class cl_tba_transactions_log extends Entity_Object {
         $this->id_oj=$id;
         
         # parameter:
-        $stpdo = $this->mypdo->prepare(cl_tba_transactions_log::t_qry_id);
+        $stpdo = $this->mypdo->prepare(self::t_qry_id);
         $stpdo->bindParam(':id', $id, PDO::PARAM_INT);
 
+        $stpdo->execute();
         $arrcode = $stpdo->errorInfo();
         if($arrcode[0] == $this->transaction_ok){//query ok
             $this->var[$this->id_oj] = $stpdo->fetchAll(PDO::FETCH_ASSOC);
             return array('result'=>10,'value'=>$this->var[$this->id_oj]);
         }else{
-            $this->toLog("Errore in cl_tba_transactions_log - query: ".cl_tba_transactions_log::t_qry_id." - param: ".$id);
+            $this->toLog("Errore in cl_tba_transactions_log - query: ".self::t_qry_id." - param: ".$id);
             return array('result'=>0);
         }
     }
@@ -69,10 +68,10 @@ class cl_tba_transactions_log extends Entity_Object {
     #   insert new transaction
     function set($var_into) {
         $errors=array();
-        $stpdo = $this->mypdo->prepare(cl_tba_transactions_log::t_qry_insert);
+        $stpdo = $this->mypdo->prepare(self::t_qry_insert);
         
         # verify parameter:
-        foreach(cl_tba_transactions_log::$var_list as $id=>$val) {
+        foreach(self::$var_list as $id=>$val) {
             if ($val['must'] && (!isset($var_into[$id]) or (strlen(trim($var_into[$id]))==0))) {
                 $errors[$id]=$val["lab"]." required";
             }
@@ -83,12 +82,13 @@ class cl_tba_transactions_log extends Entity_Object {
         if (count($errors)>0)   return array('result'=>0, 'errors'=>$errors);
         
         #   try to insert new cards
+        $stpdo->execute();
         $arrcode = $stpdo->errorInfo();
         if($arrcode[0] == $this->transaction_ok){//query ok
             $row = $stpdo->fetchAll(PDO::FETCH_ASSOC);
             return $row;
         }else{
-            $this->toLog("Errore in cl_tba_transactions_log - query: ".cl_tba_transactions_log::t_qry_insert." - param: ".$var_into);
+            $this->toLog("Errore in cl_tba_transactions_log - query: ".self::t_qry_insert." - param: ".$var_into);
             return array('result'=>0);
         }
     }
@@ -96,16 +96,17 @@ class cl_tba_transactions_log extends Entity_Object {
     #   update status
     function set_status($id, $status) {
         $errors=array();
-        $stpdo = $this->mypdo->prepare(cl_tba_cards::t_upd_status);
+        $stpdo = $this->mypdo->prepare(self::t_upd_status);
         $stpdo->bindParam(":status", $status, PDO::PARAM_STR);
         $stpdo->bindParam(":id", $id, PDO::PARAM_STR);
         
+        $stpdo->execute();
         $arrcode = $stpdo->errorInfo();
         if($arrcode[0] == $this->transaction_ok){//query ok
             $row = $stpdo->fetchAll(PDO::FETCH_ASSOC);
             return $row;
         }else{
-            $this->toLog("Errore in cl_tba_cards - query: ".cl_tba_cards::t_upd_status." - param: ".$var_into);
+            $this->toLog("Errore in cl_tba_cards - query: ".self::t_upd_status." - param: ".$var_into);
             return array('result'=>0);
         }
     }
